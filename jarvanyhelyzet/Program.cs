34cd728df952a8badbf28f41dcf2e19f8be1d241
kubarni <kubarni@stud.uni-obuda.hu>
2022-04-22 22:02:23 +0200

    Add project files.

---
+++ jarvanyhelyzet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jarvanyhelyzet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
